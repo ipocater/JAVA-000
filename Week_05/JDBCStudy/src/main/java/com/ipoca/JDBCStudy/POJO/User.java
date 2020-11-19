d714f96d6dbe0cf679e976e4287253db96d7525d
ipocater <1042663120@qq.com>
2020-11-19 17:44:14 +0800

    原生数据库优化

---
+++ Week_05/JDBCStudy/src/main/java/com/ipoca/JDBCStudy/POJO/User.java
@@ -0,0 +1,42 @@
+package com.ipoca.JDBCStudy.POJO;
+
+public class User {
+    private String id;
+
+    private String username;
+
+    private String phone;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", username='" + username + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
